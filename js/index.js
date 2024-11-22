b68bb29a729a0fbb1f40b326843b1883991a032b
MOhamed-Kamal11 <m7maaa82@gmail.com>
2024-11-22 19:41:20 +0200

    adding validity for all inputs and alerts

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@
 var siteNameInput = document.getElementById("siteName");
 var siteUrlInput = document.getElementById("siteUrl");
 var bookmarkTbody = document.getElementById("tbodyContent");
+
 var bookmarkContainers = [];
 if (localStorage.getItem("bookmarks") != null) {
   bookmarkContainers = JSON.parse(localStorage.getItem("bookmarks"));
@@ -12,15 +13,25 @@ if (localStorage.getItem("bookmarks") != null) {
 
 //Add function
 function addBookmark() {
-  var site = {
-    siteName: siteNameInput.value,
-    siteUrl: siteUrlInput.value,
-  };
-  bookmarkContainers.push(site);
-  // console.log(bookmarkContainers);
-  displayBookmark();
-  clear();
-  localStorage.setItem("bookmarks", JSON.stringify(bookmarkContainers));
+  if (allValidateInput()) {
+    var site = {
+      siteName: capitalize(siteNameInput.value),
+      siteUrl: siteUrlInput.value,
+    };
+    bookmarkContainers.push(site);
+    // console.log(bookmarkContainers);
+    displayBookmark();
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarkContainers));
+  } else {
+    Swal.fire({
+      icon: "error",
+      title: "Site Name or Url is not valid, Please follow the rules below :",
+      text: `Site name must contain at least 3 characters
+      
+      
+      &Site URL must be a valid one   EX: https://www.example.com`,
+    });
+  }
 }
 
 //display funciton
@@ -29,22 +40,25 @@ function displayBookmark() {
   for (var i = 0; i < bookmarkContainers.length; i++) {
     cartona += `
     <tr>
-            <td>${i + 1}</td>
-            <td>${bookmarkContainers[i].siteName}</td>
-            <td><a href="${
-              bookmarkContainers[i].siteUrl
-            }"><button class="btn btn-danger">Visit</button></a></td>
-            <td>
-            <button onclick='deleteBookmark(${i})'  class="btn btn-warning">
-            Delete
-            </button>
-            </td>
-            
-          </tr>
+    <td>${i + 1}</td>
+    <td>${bookmarkContainers[i].siteName}</td>
+    <td>
+    <a class="btn btn-danger" href="https://www.${
+      bookmarkContainers[i].siteUrl
+    }">
+    <i class="fa-solid fa-eye pe-2"></i> Visit</a></td>
+    <td>
+    <button onclick='deleteBookmark(${i})'  class="btn btn-warning">
+    <i class="fa-solid fa-trash-can"></i> Delete
+    </button>
+    </td>
+    
+    </tr>
     
     `;
   }
   // console.log(cartona);
+  clear();
   bookmarkTbody.innerHTML = cartona;
 }
 function clear() {
@@ -62,6 +76,7 @@ function deleteBookmark(index) {
 
 function search(inputValue) {
   // console.log(inputValue);
+  var searchList = [];
   for (var i = 0; i < bookmarkContainers.length; i++) {
     if (
       bookmarkContainers[i].siteName
@@ -77,12 +92,15 @@ function search(inputValue) {
                 inputValue,
                 `<span class="text-primary">${inputValue}</span>`
               )}</td>
-              <td><a href="${
-                bookmarkContainers[i].siteUrl
-              }"><button class="btn btn-danger">Visit</button></a></td>
+             <td>
+    <a class="btn btn-danger" href="https://www.${
+      bookmarkContainers[i].siteUrl
+    }">
+    <i class="fa-solid fa-eye pe-2"></i> Visit</a></td>
+    <td>
               <td>
               <button onclick='deleteBookmark(${i})'  class="btn btn-warning">
-              Delete
+             <i class="fa-solid fa-trash-can"></i> Delete
               </button>
               </td>
               
@@ -93,4 +111,71 @@ function search(inputValue) {
   }
 
   bookmarkTbody.innerHTML = cartona;
+}
+
+/=================================/;
+// var nameRegex = /^\w{3,20}$/;
+// var urlRegex = /^(https?:\/\/)?(w{3}\.)?\w+\.\w{2,}\/?(:\d{2,5})?(\/\w+)*$/;
+// function validateBookmarkName(nameValue) {
+//   if (nameRegex.test(nameValue)) {
+//     siteNameInput.classList.replace("is-invalid", "is-valid");
+//     nameAlert.classList.add("d-none");
+//   } else {
+//     siteNameInput.classList.add("is-invalid");
+//     nameAlert.classList.remove("d-none");
+//   }
+// }
+// function validateBookmarkUrl(urlValue) {
+//   if (urlRegex.test(urlValue)) {
+//     siteUrlInput.classList.replace("is-invalid", "is-valid");
+//     urlAlert.classList.add("d-none");
+//   } else {
+//     siteUrlInput.classList.add("is-invalid");
+//     urlAlert.classList.remove("d-none");
+//   }
+// }
+
+//  Validate Function in Global for each input
+
+function validate(regex, inputValue, alert, input) {
+  if (regex.test(inputValue)) {
+    input.classList.replace("is-invalid", "is-valid");
+    alert.classList.add("d-none");
+    return true;
+  } else {
+    input.classList.add("is-invalid");
+    alert.classList.remove("d-none");
+    return false;
+  }
+}
+
+function allValidateInput() {
+  if (
+    validate(/^\w{3,20}$/, siteNameInput.value, nameAlert, siteNameInput) &&
+    validate(
+      /^(https?:\/\/)?(w{3}\.)?\w+\.\w{2,}\/?(:\d{2,5})?(\/\w+)*$/,
+      siteUrlInput.value,
+      urlAlert,
+      siteUrlInput
+    )
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+function capitalize(str) {
+  let strArr = str.split("");
+  strArr[0] = strArr[0].toUpperCase();
+  return strArr.join("");
+}
+
+function visitWebsite(e) {
+  var websiteIndex = e.target.dataset.index;
+  var httpsRegex = /^https?:\/\//;
+  if (httpsRegex.test(bookmarks[websiteIndex].siteURL)) {
+    open(bookmarks[websiteIndex].siteURL);
+  } else {
+    open(`https://${bookmarks[websiteIndex].siteURL}`);
+  }
 }
